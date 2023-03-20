863d1634154eaea0430ea7d021238e226be8ce4e
KirzhnerMark <mark@monkeytech.co.il>
2023-03-20 12:09:47 +0200

    specified the allowed methods and headers explicitly in my app.py

--- server/app.py
+++ server/app.py
@@ -120,5 +120,13 @@ def delete_todo(id):
     db.session.commit()
     return todo_schema.jsonify(todo)
 
+@app.after_request
+def after_request(response):
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+    response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+    response.headers.add('Access-Control-Allow-Credentials', 'true')
+    return response
+
 if __name__=='__main__':
     app.run(host="0.0.0.0", port=5000, debug=True)
